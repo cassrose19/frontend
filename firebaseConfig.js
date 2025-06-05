c9592df686c375cb4af957b246a1a5eba6511ac5
cassrose19 <rose.1399@osu.edu>
2025-06-05 13:42:19 -0400

    allow users to sign up for / log in to MelodyMind with firebase

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC_Rj7M8HvZKTgy-eWMrrHmmtR1Snh81cM",
-    authDomain: "melodymind-sum2025.firebaseapp.com",
-    projectId: "melodymind-sum2025",
-    storageBucket: "melodymind-sum2025.firebasestorage.app",
-    messagingSenderId: "328293709777",
-    appId: "1:328293709777:web:374aefbd982d9c84d152f0"
-  };
